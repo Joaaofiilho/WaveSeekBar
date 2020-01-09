e57cfc1364794905ca089199e6c252f1aee958cb
joaaofiilho <joaot10112@edu.unifor.br>
2020-01-09 16:30:11 -0300

    Initial commit

---
+++ app/src/main/java/com/joaaoferreira/styledseekbar/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.joaaoferreira.styledseekbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ssb.max = 12
+        ssb.min = 2
+
+        sliderControlDistorcion.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                ssb.distorcionRange = progress + 1
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        switchPersonalizedList.setOnCheckedChangeListener { _, isChecked ->
+            ssb.personalizedList = if(isChecked) listOf("A", "B", "C", "D", "E") else null
+        }
+    }
+}
